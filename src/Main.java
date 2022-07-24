23e6dbff77833e84bcd3713bcfb61d7e5cf37906
Ekaterina Shadrintseva <kateornotkate@gmail.com>
2022-07-24 21:42:49 +0500

    2. Main class + README.md

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        SalesManager stats = new SalesManager(new int[]{10, 20, 15, -10, 5});
+        System.out.println("Максимальная продажа: " + stats.max());
+    }
+} 
